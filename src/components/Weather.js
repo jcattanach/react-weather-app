08b9e5a1f0ee5417dff98a60a3fce8533f4f696f
Jack Cattanach <jackcattanach@live.com>
2019-01-01 22:05:24 -0600

    editted errors

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,7 @@ const Weather = props => {
     <div>
       { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
       { props.temp && <p>Temperature: { props.temp }° F</p> }
-      { props.humidity && <p>Humidity: { props.humidity }</p> }
+      { props.humidity && <p>Humidity: { props.humidity }%</p> }
       { props.description && <p>Conditions: { props.description }</p> }
       { props.error && <p>{ props.error }</p> }
     </div>
